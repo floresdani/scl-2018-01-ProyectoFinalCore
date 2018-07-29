3ae034e852ae9512e9d85c91a783a55c3c1abb3a
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-07-28 21:20:28 -0400

    primer camino, form basico creado

--- src/js/users.js
+++ src/js/users.js
@@ -1,3 +1,8 @@
+document.getElementById("name").value = visitor.name;
+document.getElementById("email").value = visitor.email;
+document.getElementById("companyToVisit").value = visitor.companyToVisit;
+document.getElementById("toVisit").value = visitor.toVisit;
+
 let visitor = {
     name : "",
     email : "",
