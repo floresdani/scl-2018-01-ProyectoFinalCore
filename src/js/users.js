f5056e308a9a89a55e94ddba3739bdc318e664f3
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-07-29 17:23:03 -0400

    conectado con firebase

--- src/js/users.js
+++ src/js/users.js
@@ -3,7 +3,7 @@
 function createVisitorRecord() {
 
     visitor = {
-        name: "",
+        visitName: "",
         email: "",
         companyToVisit: "",
         toVisit: ""
@@ -15,20 +15,85 @@ function createVisitorRecord() {
     toVisit = document.getElementById("toVisit").value;
 
     visitor.name = name;
-    visitor.value = email;
+    visitor.email = email;
     visitor.companyToVisit = companyToVisit;
     visitor.toVisit = toVisit;
 
+    saveVisitor(visitor);
+
     return visitor;
+    
 }
 
 function updateToVisitSelect(event) {
     let value = event.target.value;
-    espacioIf[value]
+    var innerHTML = "";
 
     for (let i = 0; i < espacioIf[value].length; i++) {
 
-        document.getElementById("toVisit").innerHTML = `<option value="${espacioIf[value][0]}"></option>`
+        innerHTML += `<option value="${espacioIf[value][i]}">${espacioIf[value][i]}</option>`
     }
 
+    document.getElementById("toVisit").innerHTML = innerHTML;
+
 }
+
+/* (function () {
+
+    var streaming = false,
+        video = document.querySelector('#video'),
+        canvas = document.querySelector('#canvas'),
+        photo = document.querySelector('#photo'),
+        startbutton = document.querySelector('#startbutton'),
+        width = 320,
+        height = 0;
+
+    navigator.getMedia = (navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia);
+
+    navigator.getMedia(
+        {
+            video: true,
+            audio: false
+        },
+        function (stream) {
+            if (navigator.mozGetUserMedia) {
+                video.mozSrcObject = stream;
+            } else {
+                var vendorURL = window.URL || window.webkitURL;
+                video.src = vendorHTMLMediaElement.srcObject(stream);
+            }
+            video.play();
+        },
+        function (err) {
+            console.log("Ha ocurrido un error! " + err);
+        }
+    );
+
+    video.addEventListener('canplay', function (ev) {
+        if (!streaming) {
+            height = video.videoHeight / (video.videoWidth / width);
+            video.setAttribute('width', width);
+            video.setAttribute('height', height);
+            canvas.setAttribute('width', width);
+            canvas.setAttribute('height', height);
+            streaming = true;
+        }
+    }, false);
+
+    function takepicture() {
+        canvas.width = width;
+        canvas.height = height;
+        canvas.getContext('2d').drawImage(video, 0, 0, width, height);
+        var data = canvas.toDataURL('image/png');
+        photo.setAttribute('src', data);
+    }
+
+    startbutton.addEventListener('click', function (ev) {
+        takepicture();
+        ev.preventDefault();
+    }, false);
+
+})(); */
