9b409540c949a2cea06080f6add72d9196546b3a
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-07-28 21:23:51 -0400

    función registro de visita, visitorRecord

--- src/js/users.js
+++ src/js/users.js
@@ -1,11 +1,18 @@
-document.getElementById("name").value = visitor.name;
-document.getElementById("email").value = visitor.email;
-document.getElementById("companyToVisit").value = visitor.companyToVisit;
-document.getElementById("toVisit").value = visitor.toVisit;
+
 
 let visitor = {
-    name : "",
-    email : "",
-    companyToVisit : "",
-    toVisit : "",
+    name: "",
+    email: "",
+    companyToVisit: "",
+    toVisit: "",
+}
+
+function visitorRecord() {
+    
+    document.getElementById("name").value = visitor.name;
+    document.getElementById("email").value = visitor.email;
+    document.getElementById("companyToVisit").value = visitor.companyToVisit;
+    document.getElementById("toVisit").value = visitor.toVisit;
+
+    console.log("hola")
 }
