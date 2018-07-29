617a42688614295fffa706daa27b49951332c722
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-07-29 11:54:11 -0400

    reemplaso de json por un js

---
+++ src/data/dataDePrueba.js
@@ -0,0 +1,7 @@
+const iF = {
+    "aguasLab" : ["juana","marlem","karina","pedro"],
+    "laboratoria" : ["valentina", "jose tomas", "daniela", "alexandra","marcia","fabian","allison","belén"],
+    "retorantDelPato" : ["patricio", "diego"],
+    "wiskyFab": ["javier", "anthony"],
+    "mutaski": ["pedro", "juan", "diego", "martina", "rebecca", "rosa"],
+}
