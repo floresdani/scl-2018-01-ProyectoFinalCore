2df63fbd95704898fa286bc7d3c62ef12e84947b
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-07-31 12:33:24 -0400

    nuevamente cámara

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,32 @@
+// Función tomar foto 
+function takePhotoVisit() {
+
+  var video = document.querySelector('#v');
+  var canvas = document.querySelector('#c');
+  var btnPhoto = document.querySelector('#t');
+  var img = document.querySelector('#img');
+
+  navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+  if (navigator.getUserMedia) {
+    navigator.getUserMedia({ video: true }, function (stream) {
+      video.src = window.URL.createObjectURL(stream);
+      video.play();
+    }, function (e) { console.log(e); })
+  }
+  else alert('Navegador obsoleto');
+
+  video.addEventListener('loadedmetadata', function () { canvas.width = video.videoWidth; canvas.height = video.videoHeight; }, false);
+  btnPhoto.addEventListener('click', function () {
+    canvas.getContext('2d').drawImage(video, 0, 0);
+    var imgData = canvas.toDataURL('image/png');
+    img.setAttribute('src', imgData);
+  })
+
+}
+
+
+
+
 //funciones para activar / desactivar los divs
 intro = () => {
   intro.style.display = "block"
@@ -30,37 +59,6 @@ createVisitorRecord = () => {
   virtualCredential.style.display = "none";
   dashboardAdmin.style.display = "none";
 }
-
-
-/* // Función tomar foto 
-function takePhotoVisit() {
-
-  var video = document.querySelector('#v');
-  var canvas = document.querySelector('#c');
-  var btnPhoto = document.querySelector('#t');
-  var img = document.querySelector('#img');
-
-  navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
-  if (navigator.getUserMedia) {
-    navigator.getUserMedia({ video: true }, function (stream) {
-      video.src = window.URL.createObjectURL(stream);
-      video.play();
-    }, function (e) { console.log(e); })
-  }
-  else alert('Navegador obsoleto');
-
-  video.addEventListener('loadedmetadata', function () { canvas.width = video.videoWidth; canvas.height = video.videoHeight; }, false);
-  btnPhoto.addEventListener('click', function () {
-    canvas.getContext('2d').drawImage(video, 0, 0);
-    var imgData = canvas.toDataURL('image/png');
-    img.setAttribute('src', imgData);
-  })
-
-} */
-
-
-
-
 
 
 
