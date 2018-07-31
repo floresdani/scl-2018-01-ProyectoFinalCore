aa40ae9ea16fea5473d0d11466d5b408644dda8a
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-07-31 12:31:55 -0400

    comentada toda funcionalidad de la cámara hasta desarrollar el min viable

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,8 @@
-// Función tomar foto 
-function takePhotoVisit() {
-  
-    var video = document.querySelector('#v');
-    var canvas = document.querySelector('#c');
-    var btnPhoto = document.querySelector('#t');
-    var img = document.querySelector('#img');
-
-    navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
-    if (navigator.getUserMedia) {
-      navigator.getUserMedia({ video: true }, function (stream) {
-        video.src = window.URL.createObjectURL(stream);
-        video.play();
-      }, function (e) { console.log(e); })
-    }
-    else alert('Navegador obsoleto');
-
-    video.addEventListener('loadedmetadata', function () { canvas.width = video.videoWidth; canvas.height = video.videoHeight; }, false);
-    btnPhoto.addEventListener('click', function () {
-      canvas.getContext('2d').drawImage(video, 0, 0);
-      var imgData = canvas.toDataURL('image/png');
-      img.setAttribute('src', imgData);
-    })
-  
-}
-
-
-
-
 //funciones para activar / desactivar los divs
 intro = () => {
   intro.style.display = "block"
   register.style.display = "none";
-  takePhoto.style.display = "none";
+  //takePhoto.style.display = "none";
   virtualCredential.style.display = "none";
   dashboardAdmin.style.display = "none";
 }
@@ -39,7 +10,7 @@ intro = () => {
 btnRegister = () => {
   intro.style.display = "none"
   register.style.display = "block";
-  takePhoto.style.display = "none";
+  //takePhoto.style.display = "none";
   virtualCredential.style.display = "none";
   dashboardAdmin.style.display = "none";
 }
@@ -47,7 +18,7 @@ btnRegister = () => {
 btnAdmin = () => {
   intro.style.display = "none"
   register.style.display = "none";
-  takePhoto.style.display = "none";
+  //takePhoto.style.display = "none";
   virtualCredential.style.display = "none";
   dashboardAdmin.style.display = "block";
 }
@@ -55,10 +26,41 @@ btnAdmin = () => {
 createVisitorRecord = () => {
   intro.style.display = "block"
   register.style.display = "none";
-  takePhoto.style.display = "none";
+  //takePhoto.style.display = "none";
   virtualCredential.style.display = "none";
   dashboardAdmin.style.display = "none";
 }
+
+
+/* // Función tomar foto 
+function takePhotoVisit() {
+
+  var video = document.querySelector('#v');
+  var canvas = document.querySelector('#c');
+  var btnPhoto = document.querySelector('#t');
+  var img = document.querySelector('#img');
+
+  navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+  if (navigator.getUserMedia) {
+    navigator.getUserMedia({ video: true }, function (stream) {
+      video.src = window.URL.createObjectURL(stream);
+      video.play();
+    }, function (e) { console.log(e); })
+  }
+  else alert('Navegador obsoleto');
+
+  video.addEventListener('loadedmetadata', function () { canvas.width = video.videoWidth; canvas.height = video.videoHeight; }, false);
+  btnPhoto.addEventListener('click', function () {
+    canvas.getContext('2d').drawImage(video, 0, 0);
+    var imgData = canvas.toDataURL('image/png');
+    img.setAttribute('src', imgData);
+  })
+
+} */
+
+
+
+
 
 
 
