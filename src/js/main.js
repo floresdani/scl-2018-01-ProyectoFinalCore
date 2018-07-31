385373f6d1fd724711adea22dbdd05295b2fb7f8
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-07-31 11:53:59 -0400

    función takePhoto en un  button

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,31 @@
 // Función tomar foto 
-window.addEventListener('load', init);
-  function init() {
+function takePhotoVisit() {
+  
     var video = document.querySelector('#v');
     var canvas = document.querySelector('#c');
     var btnPhoto = document.querySelector('#t');
-   var img = document.querySelector('#img');
+    var img = document.querySelector('#img');
 
-   navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
-   if(navigator.getUserMedia) {
-     navigator.getUserMedia({video:true}, function(stream){
-       video.src = window.URL.createObjectURL(stream);
-       video.play();
-     }, function(e){console.log(e);})
-   }
-   else alert('Navegador obsoleto');
+    navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia({ video: true }, function (stream) {
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
+      }, function (e) { console.log(e); })
+    }
+    else alert('Navegador obsoleto');
 
-   video.addEventListener('loadedmetadata', function(){canvas.width = video.videoWidth; canvas.height = video.videoHeight;}, false);
-   btnPhoto.addEventListener('click', function(){
-     canvas.getContext('2d').drawImage(video,0,0);
-     var imgData = canvas.toDataURL('image/png');
-     img.setAttribute('src', imgData);
-   })
-  }
+    video.addEventListener('loadedmetadata', function () { canvas.width = video.videoWidth; canvas.height = video.videoHeight; }, false);
+    btnPhoto.addEventListener('click', function () {
+      canvas.getContext('2d').drawImage(video, 0, 0);
+      var imgData = canvas.toDataURL('image/png');
+      img.setAttribute('src', imgData);
+    })
+  
+}
+
+
+
 
 //funciones para activar / desactivar los divs
 intro = () => {
