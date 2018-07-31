b486a6d57e63a8b117edf7eaba245e0cf9c9cb35
mariajosesalazar <mariaj.salazarf@gmail.com>
2018-07-31 19:19:08 -0400

    pre pull

--- src/data/espacioIf.js
+++ src/data/espacioIf.js
@@ -3,12 +3,8 @@ const espacioIf = {
     "laboratoria" : ["valentina", "jose tomas", "daniela", "alexandra","marcia","fabian","allison","belén"],
     "retorantDelPato" : ["patricio", "diego"],
     "wiskyFab": ["javier", "anthony"],
-<<<<<<< HEAD:src/data/dataDePrueba.js
-    "mutaskis": ["pedro", "juan", "diego", "martina", "rebecca", "rosa"],
-=======
     "mustakis": ["pedro", "juan", "diego", "martina", "rebecca", "rosa"],
     "FabricaDeMedios": ["pedro", "juan", "diego"],
     "RoboticsLabs": ["cata","mari","lucy"],
     "elEstudio": ["cote","miranda","lore","javi"]
->>>>>>> upstream/master:src/data/espacioIf.js
 }
