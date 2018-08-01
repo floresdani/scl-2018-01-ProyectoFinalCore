99d455d8c34f9187c2fed0c5d2b043996752c87e
mariajosesalazar <mariaj.salazarf@gmail.com>
2018-07-31 22:08:06 -0400

    añadi cambios a admin.js y firebase.js

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,27 +1,26 @@
 const saveVisitor = (visitor) => {
   const newVisitorKey = firebase.database()
-    .ref().child('visitors')
-    .push().key; //crea uno vacío para obtener la llave
+      .ref().child('visitors')
+      .push().key;//crea uno vacío para obtener la llave
 
   visitor.id = newVisitorKey;
 
   return firebase.database()
-    .ref('visitors/' + newVisitorKey)
-    .set(visitor); //set actualiza el valor en esta dirección
+      .ref('visitors/' + newVisitorKey)
+      .set(visitor); //set actualiza el valor en esta dirección
 }
 
 //saveVisitor(createVisitorRecord());
 firebase.database().ref('visitors')
   .on('child_added', (newVisit) => {
-    const visitContainer = document.createElement('div');
-    dashboardAdmin.appendChild(visitContainer)
-    visitContainer.innerHTML += `<div>
-                                        <p>Visitante: ${newVisit.val().visitName}</p>
-                                        <p>Rut:${newVisit.val().rut}</p>
-                                        <p>Email:${newVisit.val().email}</p>
-                                        <p>Empresa visitada: ${newVisit.val().companyToVisit}</p>
-                                        <p>Visitó a: ${newVisit.val().toVisit}</p>
-                                     </div>`
+      const visitContainer = document.createElement('div');
+      dashboardAdmin.appendChild(visitContainer)
+      visitContainer.innerHTML +=  `<div class="divider"></div>
+                                      <div class="section">
+                                      <p>Visitante: ${newVisit.val().visitName}</p>
+                                      <p>Empresa visitada: ${newVisit.val().companyToVisit}</p>
+                                      <p>Visitó a: ${newVisit.val().toVisit}</p>
+                                   </div>`
   });
 
 /*firebase es una colección donde meto más colecciones, como las carpetas y directorias
